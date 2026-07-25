2f6e87bf45fd083c2f2405880d797fd5a0c2d117
sanjay <sanjay.sharma75691@gmail.com>
2026-07-25 18:34:46 +0530

    solution loop

--- JS_practice_QUES/function.js
+++ JS_practice_QUES/function.js
@@ -1,5 +1,4 @@
-// // node JS_practice_QUES/function.js
-
+// // JS_practice_QUES/function.js
 // // call a greets function
 // function greets() {
 //     console.log("welcome to javascript");
@@ -24,12 +23,45 @@
 // }
 // console.log(add(10, 30));
 
-// Create a function named isEven that takes one number as a parameter
-function isEven(number) {
- if (number % 2 == 0) {
-    return "Even";
-} else {
-    return "Odd";
-} 
-}
-console.log(isEven(9));
+// // Create a function named isEven that takes one number as a parameter
+// function isEven(number) {
+//  if (number % 2 == 0) {
+//     return "Even";
+// } else {
+//     return "Odd";
+// } 
+// }
+// console.log(isEven(9));
+
+// // find largest Number
+// function largest(a, b) {
+// if (a > b) {
+//     return a;
+// } else {
+//     return b;
+// }
+// }
+// console.log(largest(30,46));
+
+// // find smallest No.
+// function smallest(a,b) {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// } console.log(smallest(78,27));
+
+// // run loop 1 to n
+// function printNumber(n) {
+//     for(let i = 0; i <= n; i++) {
+//         console.log(i);
+//     }
+// } printNumber(5);
+
+// print table multiple of n
+function printTable(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i*n);
+    }
+} printTable(5);
