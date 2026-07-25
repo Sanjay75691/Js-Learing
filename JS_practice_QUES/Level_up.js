0de5b38583b212f4cd1a5cd75513fe3468faabec
sanjay <sanjay.sharma75691@gmail.com>
2026-07-25 20:09:56 +0530

    add new solution

---
+++ JS_practice_QUES/Level_up.js
@@ -0,0 +1,29 @@
+// smallest value
+let numbers = [20, 5, 90, 12, 3];
+
+function findSmallest(numbers) {
+    let smallest = numbers[0];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < smallest) {
+            smallest = numbers[i];
+        }
+    }
+
+    return smallest;
+}
+
+console.log(findSmallest(numbers));
+
+
+// find sum of even no.
+
+let numbers = [10, 15, 20, 25, 30];
+function sumEven(numbers) {
+    let sum = 0;
+    for(let i = 0; i <= numbers.length-1; i++) {
+        if (numbers[i] % 2 === 0) {
+            sum = sum + numbers[i];
+        }
+    } return sum;
+} console.log(sumEven(numbers));
